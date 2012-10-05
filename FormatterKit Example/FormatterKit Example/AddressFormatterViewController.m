1eefba010c90ffc228916f0ffdf4166fdd567525
Mattt Thompson <m@mattt.me>
2012-10-04 22:42:29 -0700

    Using Japanese numbers for address

--- FormatterKit Example/FormatterKit Example/AddressFormatterViewController.m
+++ FormatterKit Example/FormatterKit Example/AddressFormatterViewController.m
@@ -55,7 +55,7 @@ enum {
     NSMutableArray *mutableExamples = [NSMutableArray array];
     [mutableExamples addObject:[NSArray arrayWithObjects:@"633 Stag Trail Rd", @"North Caldwell", @"NJ", @"07006", @"United States", nil]];
     [mutableExamples addObject:[NSArray arrayWithObjects:@"221b Baker St", @"Paddington", @"Greater London", @"NW1 6XE", @"United Kingdom", nil]];
-    [mutableExamples addObject:[NSArray arrayWithObjects:@"渋谷１−１２−23", @"渋谷区", @"東京都", @"〒１５０−０００２", @"日本", nil]];
+    [mutableExamples addObject:[NSArray arrayWithObjects:@"渋谷１−１２−２３", @"渋谷区", @"東京都", @"〒１５０−０００２", @"日本", nil]];
     self.examples = [NSArray arrayWithArray:mutableExamples];
 
     return self;
