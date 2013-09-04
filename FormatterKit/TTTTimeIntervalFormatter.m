77f63ce1c1d2dde6a63a4a733fd2902178da2bcb
RebornSoul <nechaev.main@gmail.com>
2013-09-04 14:47:19 +0400

    Fixed localization strings, added kWordCase typedef, added method for implementing cases of different locales
    I think it would be necessary to put all language specific cases inside
    of localization keys in future.

--- FormatterKit/TTTTimeIntervalFormatter.m
+++ FormatterKit/TTTTimeIntervalFormatter.m
@@ -22,6 +22,15 @@
 
 #import "TTTTimeIntervalFormatter.h"
 
+typedef enum {
+	kWordCaseZero,
+	kWordCaseOne,
+	kWordCaseTwo,
+	kWordCaseFew,
+	kWordCaseMany,
+	kWordCaseOther
+} kWordCase;
+
 static inline NSCalendarUnit NSCalendarUnitFromString(NSString *string) {
     if ([string isEqualToString:@"year"]) {
         return NSYearCalendarUnit;
@@ -120,18 +129,29 @@ static inline NSCalendarUnit NSCalendarUnitFromString(NSString *string) {
             if ([number integerValue]) {
                 NSString *localizedString = [self localizedStringForNumber:[number integerValue] ofCalendarUnit:unit];
                 if ([languageCode isEqualToString:@"ru"]) { //Magic Russian language section
-                    NSInteger lastNumber = number.integerValue % 10;
-                    if ((lastNumber > 1 && lastNumber < 5) && (number.integerValue > 20 || number.integerValue < 10)) { //Applying special word case according to russian language rules
-                        if ([localizedString isEqualToString:@"секунд"]) localizedString = @"секунды"; else
-                        if ([localizedString isEqualToString:@"минут"]) localizedString = @"минуты"; else
-                        if ([localizedString isEqualToString:@"часов"]) localizedString = @"часа"; else
-                        if ([localizedString isEqualToString:@"дней"]) localizedString = @"дня"; else
-                        if ([localizedString isEqualToString:@"недель"]) localizedString = @"недели"; else
-                        if ([localizedString isEqualToString:@"месяцев"]) localizedString = @"месяца"; else
-                        if ([localizedString isEqualToString:@"лет"]) localizedString = @"года";
-                    } else if (lastNumber == 1) { //Plural ending with 1 always written as single
-                        localizedString = [self localizedStringForNumber:lastNumber ofCalendarUnit:unit];
-                    }
+                    NSInteger n = number.integerValue;
+					switch ([self wordCaseForNumber:n inLocale:self.locale]) {
+						case kWordCaseOne:
+						{
+							localizedString = [self localizedStringForNumber:1 ofCalendarUnit:unit];
+						}
+							break;
+						case kWordCaseFew:
+						{
+							if ([localizedString isEqualToString:@"секунд"]) localizedString = @"секунды";
+							if ([localizedString isEqualToString:@"минут"]) localizedString = @"минуты";
+							if ([localizedString isEqualToString:@"часов"]) localizedString = @"часа";
+							if ([localizedString isEqualToString:@"дней"]) localizedString = @"дня";
+							if ([localizedString isEqualToString:@"недель"]) localizedString = @"недели";
+							if ([localizedString isEqualToString:@"месяцев"]) localizedString = @"месяца";
+							if ([localizedString isEqualToString:@"лет"]) localizedString = @"года";
+						}
+							break;
+						case kWordCaseMany:
+							break;
+						default:
+							break;
+					}
                 }
                 NSString *suffix = [NSString stringWithFormat:@"%@ %@", number, localizedString];
                 if (!string) {
@@ -215,6 +235,33 @@ static inline NSCalendarUnit NSCalendarUnitFromString(NSString *string) {
 }
 
 #pragma mark -
+
+- (kWordCase)wordCaseForNumber:(NSInteger)n inLocale:(NSLocale*)locale {
+	// be bs hr ru sh sr uk
+	if ([self isLocale:locale includeAnyCodes:@[@"be",@"bs",@"hr",@"ru",@"sh",@"sr",@"uk"]]) {
+		if (((n % 10) == 1) && ((n % 100) != 11)) // n mod 10 is 1 and n mod 100 is not 11
+		{ // {One}
+			return kWordCaseOne;
+		}
+		else if ((((n % 10) >= 2 && (n % 10) <= 4)) && !((n % 100) >= 12 && (n % 100) <= 14))
+		{ // {Few}
+			return kWordCaseFew;
+		}
+		else if (((n % 10) == 0) || (((n % 10) >= 5 && (n % 10) <= 9)) || (((n % 100) >= 11 && (n % 100) <= 14)))
+		{ // {Many}
+			return kWordCaseMany;
+		}
+	}
+	return 0;
+}
+
+- (BOOL)isLocale:(NSLocale*)locale includeAnyCodes:(NSArray*)codes {
+	NSString *languageCode = [locale objectForKey:NSLocaleLanguageCode];
+	for (NSString *code in codes) {
+		if ([languageCode isEqualToString:code]) return YES;
+	}
+	return NO;
+}
 
 - (NSString *)localizedIdiomaticDeicticExpressionForComponents:(NSDateComponents *)components {
     NSString *languageCode = [self.locale objectForKey:NSLocaleLanguageCode];
