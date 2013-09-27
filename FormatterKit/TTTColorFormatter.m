f031c9045ecdf2dc72d7423aa472b1997c31254b
Manuel Burghard <manuel.burghard@gmail.com>
2013-09-27 18:03:51 +0200

    Added missing implementation for colorFromCMYKString:.

--- FormatterKit/TTTColorFormatter.m
+++ FormatterKit/TTTColorFormatter.m
@@ -153,7 +153,24 @@ static void TTTGetHSLComponentsFromColor(UIColor *color, CGFloat *hue, CGFloat *
 }
 
 - (UIColor *)colorFromCMYKString:(NSString *)string {
-    return nil;
+    NSScanner *scanner = [NSScanner scannerWithString:string];
+    scanner.charactersToBeSkipped = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    
+    CGFloat c, m, y, k;
+    
+    [scanner scanFloat:&c];
+    [scanner scanFloat:&m];
+    [scanner scanFloat:&y];
+    [scanner scanFloat:&k];
+    
+    c *= 0.01f;
+    m *= 0.01f;
+    y *= 0.01f;
+    k *= 0.01f;
+    
+    CGFloat dk = 1.0f - k;
+    
+    return [UIColor colorWithRed:(1.0f - c) * dk green:(1.0f - m) * dk blue:(1.0f - y) * dk alpha:1.0f];
 }
 
 #pragma mark -
