02afdae3d3bdfc52c0ac44789892b6b5d8f683e0
Tomaz Kragelj <tkragelj@gmail.com>
2013-11-13 15:24:22 +0100

    Added OS X example target for testing build errors.
    Mac app doesn't do anything besides showing the main window. But it's useful to test OS X builds for potential compiler warnings or errors.

---
+++ Example/FormatterKit OS X Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FormatterKit OS X Example
+//
+//  Created by Tomaz Kragelj on 13.11.13.
+//  Copyright (c) 2013 Gowalla. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example/FormatterKit OS X Example/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  FormatterKit OS X Example
+//
+//  Created by Tomaz Kragelj on 13.11.13.
+//  Copyright (c) 2013 Gowalla. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Nothing here, just a target to make sure it builds without warnings.
+}
+
+@end
