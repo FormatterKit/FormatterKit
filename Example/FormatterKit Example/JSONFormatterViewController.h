04f96c6890d8110481301d24c696bbd84d28057d
griffin_stewie <panterathefamilyguy@gmail.com>
2014-03-18 21:30:02 +0900

    Add TTTJSONFormatter

---
+++ Example/FormatterKit Example/JSONFormatterViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSONFormatterViewController.h
+//
+//
+//  Created by griffin-stewie on 2014/03/18.
+//
+//
+
+#import "FormatterViewController.h"
+
+@interface JSONFormatterViewController : FormatterViewController
+
+@end
