aca741de2e56f05a263c1d931187dce81183c6b9
MeXx <mexx@devsub.net>
2015-05-06 15:18:21 +0200

    Add framework targets to support carthage
    FormatterKit.xcodeproj was created in the root folder. This project has targets to build frameworks for iOS and OSX. The schemes for these targets are shared so that FormatterKit can be included using carthage.

---
+++ FormatterKit/FormatterKit.h
@@ -0,0 +1,27 @@
+//
+//  FormatterKit.h
+//  FormatterKit
+//
+//  Created by Markus Chmelar on 06/05/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormatterKit.
+FOUNDATION_EXPORT double FormatterKitVersionNumber;
+
+//! Project version string for FormatterKit.
+FOUNDATION_EXPORT const unsigned char FormatterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormatterKit/PublicHeader.h>
+
+#import <FormatterKit/TTTAddressFormatter.h>
+#import <FormatterKit/TTTArrayFormatter.h>
+#import <FormatterKit/TTTColorFormatter.h>
+#import <FormatterKit/TTTLocationFormatter.h>
+#import <FormatterKit/TTTNameFormatter.h>
+#import <FormatterKit/TTTOrdinalNumberFormatter.h>
+#import <FormatterKit/TTTTimeIntervalFormatter.h>
+#import <FormatterKit/TTTUnitOfInformationFormatter.h>
+#import <FormatterKit/TTTURLRequestFormatter.h>
