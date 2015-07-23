FormatterKit/FormatterKit

commit 05aeefc5d4f2f60c323ded6410dc0cb461271b92

[Fix #140] - Fix NSCalendarUnitCompareSignificance method check

FormatterKit/TTTTimeIntervalFormatter.m
@@ -66,7 +66,7 @@ static inline NSCalendarUnit NSCalendarUnitFromString(NSString *string) {
 
 static inline NSComparisonResult NSCalendarUnitCompareSignificance(NSCalendarUnit a, NSCalendarUnit b) {
     if ((a == TTTCalendarUnitWeek) ^ (b == TTTCalendarUnitWeek)) {
-        if (a == TTTCalendarUnitWeek) {
+        if (b == TTTCalendarUnitWeek) {
             switch (a) {
                 case TTTCalendarUnitYear:
                 case TTTCalendarUnitMonth:

commit 241feb648bf5da17dd3dc38421428d64dbd4f000

Fixes bug introduced in 05aeefc5d4f2f60c323ded6410dc0cb461271b92.

FormatterKit/TTTTimeIntervalFormatter.m
@@ -66,8 +66,8 @@ static inline NSCalendarUnit NSCalendarUnitFromString(NSString *string) {
 
 static inline NSComparisonResult NSCalendarUnitCompareSignificance(NSCalendarUnit a, NSCalendarUnit b) {
     if ((a == TTTCalendarUnitWeek) ^ (b == TTTCalendarUnitWeek)) {
-        if (b == TTTCalendarUnitWeek) {
-            switch (a) {
+        if (a == TTTCalendarUnitWeek) {
+            switch (b) {
                 case TTTCalendarUnitYear:
                 case TTTCalendarUnitMonth:
                     return NSOrderedAscending;