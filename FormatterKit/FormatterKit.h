8eb4a6aee08bf7ef243cdf999961b55c92e23670
Markus Chmelar <mexx@devsub.net>
2016-03-26 11:57:53 +0100

    Create Project that builds a Framework for FormatterKit, share the build schemes to enable building via Carthage

---
+++ FormatterKit/FormatterKit.h
@@ -0,0 +1,27 @@
+//
+//  FormatterKit.h
+//  FormatterKit
+//
+//  Created by Markus Chmelar on 26/03/16.
+//  Copyright © 2016 FormatterKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormatterKit.
+FOUNDATION_EXPORT double FormatterKitVersionNumber;
+
+//! Project version string for FormatterKit.
+FOUNDATION_EXPORT const unsigned char FormatterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormatterKit/PublicHeader.h>
+
+#import <FormatterKit/TTTAddressFormatter.h>
+#import <FormatterKit/TTTArrayFormatter.h>
+#import <FormatterKit/TTTColorFormatter.h>
+#import <FormatterKit/TTTLocationFormatter.h>
+#import <FormatterKit/TTTNameFormatter.h>
+#import <FormatterKit/TTTOrdinalNumberFormatter.h>
+#import <FormatterKit/TTTTimeIntervalFormatter.h>
+#import <FormatterKit/TTTUnitOfInformationFormatter.h>
+#import <FormatterKit/TTTURLRequestFormatter.h>
