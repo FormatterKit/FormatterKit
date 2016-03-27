8800875fb7924e8517ce582e17257aa9eb304144
Laszlo Teveli <laszlo.teveli@skyscanner.net>
2016-03-27 22:02:14 +0200

    Removing unused import statement

--- FormatterKit/TTTTimeIntervalFormatter.m
+++ FormatterKit/TTTTimeIntervalFormatter.m
@@ -21,7 +21,6 @@
 // THE SOFTWARE.
 
 #import "TTTTimeIntervalFormatter.h"
-#import "TTTArrayFormatter.h"
 
 #if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
     #define TTTCalendarUnitYear NSCalendarUnitYear
