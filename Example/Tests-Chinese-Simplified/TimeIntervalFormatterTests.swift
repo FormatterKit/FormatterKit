877d39e790156559b5f638b6cf40a35ae07a18db
Elethom Hunter <elethomhunter@gmail.com>
2016-05-01 01:32:49 +0800

    Update unit tests for zh_CN

--- Example/Tests-Chinese-Simplified/TimeIntervalFormatterTests.swift
+++ Example/Tests-Chinese-Simplified/TimeIntervalFormatterTests.swift
@@ -39,13 +39,24 @@ class TTTTimeIntervalFormatterTests: XCTestCase {
 
     func testIdiomaticPast() {
         formatter.usesIdiomaticDeicticExpressions = true
+        
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24), "昨天")
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24 * 2), "前天")
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24 * 7), "上周")
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24 * 31), "上月")
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24 * 366), "去年")
+        XCTAssertEqual(formatter.stringForTimeInterval(-3600 * 24 * 366 * 2), "前年")
+    }
+    
+    func testIdiomaticFuture() {
+        formatter.usesIdiomaticDeicticExpressions = true
 
-        XCTAssertEqual(formatter.stringForTimeInterval(-1), "1秒前")
-        XCTAssertEqual(formatter.stringForTimeInterval(-100), "1分钟前")
-        XCTAssertEqual(formatter.stringForTimeInterval(-10000), "2小时前")
-        XCTAssertEqual(formatter.stringForTimeInterval(-100000), "1天前")
-        XCTAssertEqual(formatter.stringForTimeInterval(-200000), "2天前")
-        XCTAssertEqual(formatter.stringForTimeInterval(-10000000), "3月前")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24), "明天")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24 * 2), "后天")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24 * 7), "下周")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24 * 31), "下月")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24 * 366), "明年")
+        XCTAssertEqual(formatter.stringForTimeInterval(3600 * 24 * 366 * 2), "后年")
     }
     
 }
