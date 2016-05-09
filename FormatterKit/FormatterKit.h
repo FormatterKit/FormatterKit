6233e7c4833a231041d0ca5104ba40c7607aa65b
Paulo Andrade <pfandrade@gmail.com>
2016-05-09 04:49:56 +0100

    Umbrella header now imports Foundation instead of UIKit

--- FormatterKit/FormatterKit.h
+++ FormatterKit/FormatterKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 FormatterKit. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for FormatterKit.
 FOUNDATION_EXPORT double FormatterKitVersionNumber;
