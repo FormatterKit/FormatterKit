4bf02adc32f4d17c1e2cf2d94acd7afc863b9fc5
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-05-14 11:18:46 -0700

    Initial tests for TTTArrayFormatter

---
+++ Example/Tests-English/ArrayFormatterTests.swift
@@ -0,0 +1,57 @@
+//
+//  ArrayFormatterTests.swift
+//  FormatterKit Example
+//
+//  Created by Victor Ilyukevich on 5/14/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+import XCTest
+import FormatterKit
+
+class ArrayFormatterTests: XCTestCase {
+
+    var formatter: TTTArrayFormatter!
+
+    let dogCatParrot = ["Dog", "Cat", "Parrot"]
+
+    override func setUp() {
+        super.setUp()
+        formatter = TTTArrayFormatter()
+    }
+
+    // MARK: Tests
+
+    func testDefault() {
+        let result = formatter.stringFromArray(dogCatParrot)
+        let expected = "Dog, Cat, and Parrot"
+        
+        XCTAssertEqual(result, expected)
+        // TODO
+        // XCTAssertEqual(formatter.arrayFromString(expected) as! [String], dogCatParrot)
+    }
+
+    func testAbbreviatedConjunction() {
+        formatter.usesAbbreviatedConjunction = true
+
+        let result = formatter.stringFromArray(dogCatParrot)
+        let expected = "Dog, Cat, & Parrot"
+
+        XCTAssertEqual(result, expected)
+        // TODO
+        // XCTAssertEqual(formatter.arrayFromString(expected) as! [String], dogCatParrot)
+
+    }
+
+    func testWithoutOxformComma() {
+        formatter.usesSerialDelimiter = false
+
+        let result = formatter.stringFromArray(dogCatParrot)
+        let expected = "Dog, Cat and Parrot"
+
+        XCTAssertEqual(result, expected)
+        // TODO
+        // XCTAssertEqual(formatter.arrayFromString(expected) as! [String], dogCatParrot)
+    }
+
+}
