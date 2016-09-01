17d914fd2e0bf64fa4cbf37cbf60e5f090757704
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-09-01 11:39:43 +0300

    Don't sign build for watchOS integration tests
    Fixes: https://travis-ci.org/mattt/FormatterKit/jobs/156439846
    https://travis-ci.org/mattt/FormatterKit/jobs/156199499#L333

--- IntegrationTests/CocoaPods-watchOS/run.sh
+++ IntegrationTests/CocoaPods-watchOS/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 bundle exec pod install
-xcodebuild -workspace Example.xcworkspace -scheme Example build | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild -workspace Example.xcworkspace -scheme Example build CODE_SIGNING_ALLOWED=NO CODE_SIGNING_REQUIRED=NO | xcpretty && exit ${PIPESTATUS[0]}
 
