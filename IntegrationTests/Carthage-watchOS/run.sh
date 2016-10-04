53353f99c0db4f7df141c6cea72bdb738ab84333
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2016-10-04 15:32:31 -0700

    Don't sign watchOS build in Carthage test

--- IntegrationTests/Carthage-watchOS/run.sh
+++ IntegrationTests/Carthage-watchOS/run.sh
@@ -5,5 +5,5 @@ echo "git \"file://$(cd `pwd`/../.. && pwd)\" \"`git rev-parse HEAD`\"" > Cartfi
 echo "Carthage `carthage version`"
 carthage update --platform 'watchos' --verbose
 
-xcodebuild -scheme 'watchOSApp' | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild -scheme 'watchOSApp' build CODE_SIGNING_ALLOWED=NO CODE_SIGNING_REQUIRED=NO | xcpretty && exit ${PIPESTATUS[0]}
 
